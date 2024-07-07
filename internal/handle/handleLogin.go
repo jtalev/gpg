a8a68e3f04db7a71b05259b07170451a59fd64e4
j-talev <j.talev@outlook.com>
2024-07-07 21:04:58 +1000

    HandleValidateLogin responds with error messages, ui displays

--- internal/handle/handleLogin.go
+++ internal/handle/handleLogin.go
@@ -2,16 +2,53 @@ package handle
 
 import (
 	"context"
+	"fmt"
 	"gpg/portal/internal/localdb"
+	"gpg/portal/internal/validation"
 	"html/template"
+	"log"
 	"net/http"
 )
 
-func ServeIndex(ctx context.Context, db localdb.Db) http.Handler {
+func ServeLogin(ctx context.Context, db localdb.Db) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			log.Println("serving login page")
 			tmpl := template.Must(template.ParseFiles("../../web/pages/login.html"))
-			tmpl.Execute(w, nil)
+			if err := tmpl.Execute(w, nil); err != nil {
+				http.Error(w, "error executing template", http.StatusInternalServerError)
+			}
+		},
+	)
+}
+
+func HandleValidateLogin(ctx context.Context, db localdb.Db) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			log.Println("handling login validation request")
+			if err := r.ParseForm(); err != nil {
+				log.Printf("error parsing form: %v", err)
+				http.Error(w, "error parsing form", http.StatusBadRequest)
+				return
+			}
+			username := r.FormValue("username")
+			password := r.FormValue("password")
+
+			result := validation.ValidateLogin(db.Ur, username, password)
+			// if result.Msg == "" {
+			// 	handle redirect to dashboard
+			// }
+			var html string
+			if result.Msg != "" {
+				html = fmt.Sprintf(`<p class="err" id="err">*%s</p>`, result.Msg)
+			}
+			if _, err := w.Write([]byte(html)); err != nil {
+				log.Printf("error writing response: %v", err)
+				http.Error(w, "error writing response", http.StatusInternalServerError)
+				return
+			}
+
+			log.Println("login validation request handled")
 		},
 	)
 }
